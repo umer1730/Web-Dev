72bff42fc7e2384ae98d38ebed5caf4d741eae69
UMER YASEEN <umery1730@gmail.com>
2026-07-03 15:22:17 +0500

    create info about student

--- task-1/src/App.jsx
+++ task-1/src/App.jsx
@@ -4,21 +4,27 @@ function App() {
   return (
     <>
       <Student 
+        icon={<span>🎓</span>}
         name="Ali" 
         age={22} 
+        city = "Lahore"
         isStudent = {true}
         skills = {["React","Python","C++"]}/>
       
-      <Student 
+      <Student
+        icon={<span>🎓</span>} 
         name="Asad" 
         age={24}
+        city = "Islamabad"
         isStudent = {true}
         skills = {["Java","React","Flutter"]} 
       />
       
       <Student 
+        icon={<span>🎓</span>}
         name="Saim" 
         age={25} 
+        city= "Multan"
         isStudent = {true}
         skills = {["C++","JS","C#"]}
       />
