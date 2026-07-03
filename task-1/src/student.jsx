8d81846093a175b0ec0013d64e76f0c9d0617151
UMER YASEEN <umery1730@gmail.com>
2026-07-03 15:22:39 +0500

    sudent file

--- task-1/src/student.jsx
+++ task-1/src/student.jsx
@@ -1,14 +1,25 @@
 
 
-function Student(props) {
+function Student({icon,name,age,city,isStudent,skills}) {
   return (
     <>
-      <h2>Name: {props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Student: {props.isStudent ? "Yes": "No"}</p>
-      <p>Skills: {props.skills.join(", ")}</p>
+        {icon}
+      <h2>Name: {name}</h2>
+      <p>Age: {age}</p>
+      <p>City: {city}</p>
+      <p>Student: {isStudent ? "Yes": "No"}</p>
+      <p>Skills: {skills.join(", ")}</p>
       <hr />
     </>
+      /*  we can also use this approach but in this approach we write props before name , age etc so we use above approach  
+       <>
+        <h2>Name: {props.name}</h2>
+        <p>Age: {props.age}</p>
+        <p>Student: {props.isStudent ? "Yes": "No"}</p>
+        <p>Skills: {props.skills.join(", ")}</p>
+        <hr />
+        </>
+       */ 
   );
 }
 
