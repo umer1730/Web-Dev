0329dbcbd11f0f3eb489a93ffa7f06fc5bc36594
UMER YASEEN <umery1730@gmail.com>
2026-07-04 14:37:18 +0500

    create a button

---
+++ React/Day-2/task/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>{count}</h1>
+
+      <button onClick={() => {setCount(count+1);
+      }}
+      >Increase</button>
+    </>
+  )
+}
+
+export default App
