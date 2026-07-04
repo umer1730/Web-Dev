de3d246a527d97b67ef673abfb4ed62e53c1980f
UMER YASEEN <umery1730@gmail.com>
2026-07-04 20:00:22 +0500

    using onChange method

---
+++ React/Day-2/task-3/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [name, setName] = useState("")
+
+  return (
+    
+    <>
+    
+      <input 
+        type = "text"
+        onChange={(e) =>setName(e.target.value)}/>
+      <h2>{name}</h2>
+      
+    </>
+  )
+}
+
+export default App
