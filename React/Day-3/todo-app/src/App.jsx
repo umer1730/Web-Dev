c423f04ad5ca729def5507ed7eb72c36419647a6
UMER YASEEN <umery1730@gmail.com>
2026-07-06 04:32:43 +0500

    add todo to the list

--- React/Day-3/todo-app/src/App.jsx
+++ React/Day-3/todo-app/src/App.jsx
@@ -1,30 +1,41 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const[task,setTask] = useState("")
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    setTodos([...todos, task]);
+    setTask("");
+  }
+
+  function deleteTodo(indexTodelete){
+    const updatedTodos = todos.filter((todo,index) =>{
+      return index !== indexTodelete;
+    })
+  }
 
   return (
-    <div className = "container">
+    <div className="container">
       <h1>Todo App</h1>
 
-      <input type= "text"
-      placeholder = "Enter a task"
-      value = {task}
-      onChange={(e) => setTask(e.target.value)}
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
       />
 
-      <button>Add</button>
-      <h3>Current Task</h3>
-      <p>{task}</p>
+      <button onClick={addTodo}>Add</button>
 
       <ul>
-        <li>Learn React</li>
-        <li>Build Todo App</li>
-        <li>Practice useState</li>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
