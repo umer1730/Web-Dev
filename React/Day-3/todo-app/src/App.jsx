003cc967a7d2a80e307d2d3f12dd17121880999d
UMER YASEEN <umery1730@gmail.com>
2026-07-06 04:46:58 +0500

    yes No in todo app

--- React/Day-3/todo-app/src/App.jsx
+++ React/Day-3/todo-app/src/App.jsx
@@ -5,16 +5,41 @@ function App() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState([]);
 
+  // Add Todo
   function addTodo() {
     if (task.trim() === "") return;
 
-    setTodos([...todos, task]);
+    setTodos([
+      ...todos,
+      {
+        text: task,
+        completed: false,
+      },
+    ]);
+
     setTask("");
   }
 
+  // Delete Todo
   function deleteTodo(indexToDelete) {
     const updatedTodos = todos.filter((todo, index) => {
       return index !== indexToDelete;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  // Toggle Complete
+  function toggleComplete(indexToToggle) {
+    const updatedTodos = todos.map((todo, index) => {
+      if (index === indexToToggle) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
     });
 
     setTodos(updatedTodos);
@@ -36,7 +61,19 @@ function App() {
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
-            {todo}
+            <span
+              onClick={() => toggleComplete(index)}
+              style={{
+                textDecoration: todo.completed
+                  ? "line-through"
+                  : "none",
+                cursor: "pointer",
+                flex: 1,
+              }}
+            >
+              {todo.completed ? "Yes " : "No "}
+              {todo.text}
+            </span>
 
             <button onClick={() => deleteTodo(index)}>
               Delete
