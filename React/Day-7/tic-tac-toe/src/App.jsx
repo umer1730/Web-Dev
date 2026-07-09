f6016ab62a6c7db7aedf987e35d556da0c8c6337
UMER YASEEN <umery1730@gmail.com>
2026-07-10 03:10:06 +0500

    add character

--- React/Day-7/tic-tac-toe/src/App.jsx
+++ React/Day-7/tic-tac-toe/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <h1>Tic Tac Toe</h1>
-
       <Board />
     </>
   );
--- React/Day-7/tic-tac-toe/src/components/board.jsx
+++ React/Day-7/tic-tac-toe/src/components/board.jsx
@@ -1,20 +1,69 @@
-import Square from "./square";
+import { useState } from "react";
+import Square from "./Square";
 
 function Board() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXTurn, setIsXTurn] = useState(true);
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  function checkWinner(board) {
+    for (let combo of winningCombinations) {
+      const [a, b, c] = combo;
+
+      if (
+        board[a] &&
+        board[a] === board[b] &&
+        board[a] === board[c]
+      ) {
+        return board[a];
+      }
+    }
+
+    return null;
+  }
+
+  const winner = checkWinner(board);
+
+  function handleClick(index) {
+    if (winner || board[index] !== null) return;
+
+    const newBoard = [...board];
+
+    newBoard[index] = isXTurn ? "X" : "O";
+
+    setBoard(newBoard);
+
+    setIsXTurn(!isXTurn);
+  }
+
   return (
-    <div className="board">
-      <Square />
-      <Square />
-      <Square />
+    <>
+      {winner && (
+        <h2 className="winner">
+          🏆 Winner: {winner}
+        </h2>
+      )}
 
-      <Square />
-      <Square />
-      <Square />
-
-      <Square />
-      <Square />
-      <Square />
-    </div>
+      <div className="board">
+        {board.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onClick={() => handleClick(index)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
